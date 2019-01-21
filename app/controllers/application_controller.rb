4f119e2ab6f785318d2a000f69641a5a591ca9df
Anuj Panchal <anujpan1@gmail.com>
2019-01-21 22:40:40 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
   
   post '/articles' do
     @article = Article.create(params)
-    redirect '/articles/'
+    redirect '/articles/:id'
   end
   
 end
