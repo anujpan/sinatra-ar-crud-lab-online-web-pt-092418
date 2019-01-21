b1d91b970ed3c82878638d603557a3b9b6aca443
Anuj Panchal <anujpan1@gmail.com>
2019-01-21 23:13:53 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,7 +37,7 @@ class ApplicationController < Sinatra::Base
   
   patch 'articles/:id' do
     @article = Article.find(params[:id])
-    @article = Article.title 
+    @article.title = params[:title]
     erb :edit
   end
   
