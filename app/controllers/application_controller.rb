1a2084dfcb487cb648d22c9c15b59b508d1a4184
Anuj Panchal <anujpan1@gmail.com>
2019-01-21 23:15:01 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,7 +35,7 @@ class ApplicationController < Sinatra::Base
     erb :edit
   end
   
-  post 'articles/:id' do
+  post '/articles/:id' do
     @article = Article.find(params[:id])
     @article.title = params[:title]
     @article.content = params[:content]
